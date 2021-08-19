97408be96fe1a5b97b93db3216c68ce5d2f1ede4
Boosio(Boosyou) <developerman.it@gmail.com>
2021-08-19 19:50:11 +0430

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import mysql.connector
-cnx = mysql.connector.connect(user='pyhton_user', password='6610557',
+cnx = mysql.connector.connect(user='pyhton_user', password='******',
                               host='127.0.0.1',
                               database='python_test')
 #       INsertDataBase
